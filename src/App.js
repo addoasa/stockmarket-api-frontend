244f3234e544f8fe187ee14f428d9a01cb261139
Kwadwo <addoasa94@gmail.com>
2019-12-27 13:25:26 -0500

    App is fetching from Api and has stored the first 20 stocks in state object.

--- src/App.js
+++ src/App.js
@@ -4,14 +4,36 @@ import ContentDisplay from './components/ContentDisplay.js';
 
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      stocksToRender: {},
+    };
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:4000/stocks?limit=20&skip=0&sort=name&increasing=true', {
+      method: 'get',
+		  headers: {
+        'content-type': 'application/json',
+        Accept: 'application/json',
+      },
+    })
+      .then((gotStockData) => gotStockData.json())
+      .then((readableStockData) => {
+        this.setState({ stocksToRender: readableStockData });
+      });
+  }
+
   render() {
+    console.log('RenderedStocks', this.state.stocksToRender);
     return (
       <div className="App p-4">
         <Header />
         <ContentDisplay />
       </div>
     );
-  };
-};
+  }
+}
 
 export default App;
