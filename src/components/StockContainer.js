012362af2967ff5950f1a4869b4b48d6de3d7f3a
Kwadwo <addoasa94@gmail.com>
2019-12-27 14:34:13 -0500

    Creates a <StockListing /> component for every stock fetched

--- src/components/StockContainer.js
+++ src/components/StockContainer.js
@@ -13,11 +13,15 @@ class StockContainer extends React.Component {
     if (!this.props.stocksToRender.stocks) {
       stockListings = null;
     } else {
-      stockListings = this.props.stocksToRender.stocks.map((stock, index) => (stock));
+      stockListings = this.props.stocksToRender.stocks.map((stock, index) =>
+      // create a <StockListing /> component for every object in stockListings array
+        <StockListing key={`stock${index}`} stockData={stock} />);
     }
     console.log(stockListings);
     return (
-      <></>
+      <>
+        {stockListings}
+      </>
       // <>{this.props.isSearching ? searchResultListings : stockListings}</>
     );
   }
