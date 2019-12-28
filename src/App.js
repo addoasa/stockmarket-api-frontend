549317019ee9b27674938e06bd9305c283d212cb
Kwadwo <addoasa94@gmail.com>
2019-12-28 13:30:35 -0500

    Implements pagination feature upon scrolling to the bottom of the page. Added some comments.

--- src/App.js
+++ src/App.js
@@ -12,13 +12,16 @@ class App extends React.Component {
       searchTerm: '',
       searchResultsToRender: {},
       isSearching: false,
-      windowHeight: 0,
-      totalHeight: 0,
-      scrollAmount: 0,
+      windowHeight: null,
+      totalHeight: 9,
+      scrollAmount: null,
       haveReachedBottom: false,
+      paginationSkip : 20,
     };
     this.searchForStock = this.searchForStock.bind(this);
     this.checkHeight = this.checkHeight.bind(this);
+    this.checkIfUserReachesBottom = this.checkIfUserReachesBottom.bind(this);
+    this.getMoreStocks = this.getMoreStocks.bind(this);
   }
 
   componentDidMount() {
@@ -45,7 +48,10 @@ class App extends React.Component {
     // Monitor how much the user has scrolled 
     // --------------------------------------------
     // firing this event will call funtion onscroll for pagination
-    window.addEventListener('scroll', this.checkHeight);
+    window.addEventListener('scroll', ()=>{
+      this.checkHeight();
+      this.checkIfUserReachesBottom();
+    });
   }
  
 
@@ -58,7 +64,7 @@ class App extends React.Component {
       },
     })
       .then((gotSearchResults) => {
-        gotSearchResults.json();
+        return gotSearchResults.json();
       })
       .then((readableSearchResults) => {
         this.setState({ searchResultsToRender: readableSearchResults });
@@ -69,7 +75,9 @@ class App extends React.Component {
     // --------------------------------------------
     // Store scroll amount, current viewport and document height in state
     // --------------------------------------------
+    // This stored data will be used to determine where the user is on the page
     // This function will fire whenever a user scrolls
+
     this.setState({
       windowHeight: document.documentElement.clientHeight,
       totalHeight: document.documentElement.offsetHeight,
@@ -77,11 +85,9 @@ class App extends React.Component {
     });
   }
 
-
-  render() {
-    // console.log('RenderedStocks', this.state.stocksToRender);
+  checkIfUserReachesBottom(){
     // --------------------------------------------
-    // If the different between the total height and the scroll amount equals our viewport
+    // If the difference between the total height and the scroll amount equals our viewport
     // we have hit the bottom of the page.
     // (This is a temporary solution does not work with mobile yet)
     // --------------------------------------------
@@ -95,10 +101,61 @@ class App extends React.Component {
 
     if ((totalHeight - scrolled) === viewport) {
       console.log('You have reached the bottom of the page');
+      // Allow for some time to pass before proceeding to fetch for more data
+      setTimeout(()=>{
+        this.getMoreStocks();
+      },2000)
     }
-    
-    // --------------------------------------------
+  }
 
+  getMoreStocks(){
+    // --------------------------------------------
+    // Fetch next 20 items from stock api
+    // --------------------------------------------
+    // If we are here, that should mean that the user has scrolled to the bottom of the page
+    // Use a field called paginationSkip in state to keep track of the number of 
+    // results we want to skip in order to get the next set of stocks we want
+    fetch(`http://localhost:4000/stocks?limit=20&skip=${this.state.paginationSkip}&sort=name&increasing=true`, {
+    method: 'get',
+    headers: {
+      'content-type': 'application/json',
+      Accept: 'application/json',
+    },
+  })
+    .then((gotMoreStocks) => {
+      return gotMoreStocks.json();
+    })
+    .then((readableNewStocks) => {
+      // --------------------------------------------
+      // Update stocks in state with data we've just fetched
+      // --------------------------------------------
+      
+      // Make a clone of the current stocks object in state
+      let cloneOfCurrentStocksInState = JSON.parse(JSON.stringify(this.state.stocksToRender));
+      
+      // Create a new array that merges the stocks currently in state with the new stocks 
+      // that we've recieved from this pagination fetch
+      const updatedStocksToRender = cloneOfCurrentStocksInState.stocks.concat(readableNewStocks.stocks);
+
+      // update the stocks array which is what is dynamically being used to create new <StockListing /> components
+      cloneOfCurrentStocksInState.stocks = updatedStocksToRender;
+
+      // update state object with modified clone
+      this.setState((state, props) => {
+        return {stocksToRender: cloneOfCurrentStocksInState};
+      });
+    });
+    // increment the pagination count in preparation for the next time
+    this.setState((state, props) => {
+      return {paginationSkip: state.paginationSkip + 20};
+    });  
+  }
+ 
+
+  render() {
+    console.log('RenderedStocks', this.state.stocksToRender);
+   
+  
     return (
       <div className="App p-0">
         <Header
